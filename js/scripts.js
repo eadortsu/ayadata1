59c0d6722a6d323d4184adaa4e251c06a5b60612
eadortsu <mail@eadortsu.com>
2021-08-21 07:59:52 +0000

    home update

--- js/scripts.js
+++ js/scripts.js
@@ -230,15 +230,74 @@ jQuery(function ($) {
     lazyLoad: true,
     items: 1,
   });
+  $(".companies-slider").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    touchDrag: true,
+    mouseDrag: true,
+    nav: false,
+    dots: true,
+    autoplayTimeout: 6000,
+    autoplaySpeed: 1200,
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    responsive: {
+      0: {
+        items: 2
+      },
+      480: {
+        items: 2
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 2
+      },
+      1200: {
+        items: 2
+      }
+    }
+  });
 
   $('.client-testimonial').owlCarousel({
     loop: true,
     margin: 30,
     nav: true,
+    dots: true,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      800: {
+        items: 2
+      },
+      1200: {
+        items: 3
+      }
+    }
+  });
+
+  $('.client-patners').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
     dots: false,
     responsiveClass: true,
     autoplay: true,
-    autoplayHoverPause: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
     lazyLoad: true,
     responsive: {
       0: {
